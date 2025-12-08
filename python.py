59c0f97ff128173ddd4bfa78254342440e18afb5
jlovejr-a11y <jlovejr@icstars.org>
2025-12-07 19:26:28 -0600

    Update python.py

--- python.py
+++ python.py
@@ -3,3 +3,47 @@ print("Wassup!") # this is the firt line of code was it easy?
 #"Someone is always needed to debug code
 #"debuggers make averaging around $90k-$130k+ USD depending on location/experience"
 #the last three lines are the recommended for exapmles 
+
+# Variables
+name = "Alex"
+age = 13
+favorite_color = "blue"
+
+# Print them in a sentence
+print("My name is", name, "I am", age, "years old and I love", favorite_color)
+# This is a simple program to introduce variables and printing in Python
+# End of week TWO
+
+
+
+# Ask for user input
+name = input("What is your name? ")
+age = input("How old are you? ")
+
+print("Hi", name, "you are", age, "years old!")
+# This program introduces user input in Python
+#explain the input() functionin the terminal
+# End of week THREE
+
+
+# Check if number is even or odd
+number = int(input("Enter a number: "))
+
+if number % 2 == 0: #'% 2 == 0' checks if the number is divisible by 2 with no remainder
+    print("The number is even.")
+else: # if not even, it must be odd
+    print("The number is odd.")
+
+# This program introduces conditional statements in Python
+# End of week FOUR
+
+
+
+# Print numbers 1 to 10
+for i in range(1, 11): #i is a variable that takes each value from 1 to 10
+    print(i)
+
+# While loop example
+count = 0
+while count < 5:
+    print("Hello!")
